aac883e08cc43ded51d6cc60e5455bdc085e20f0
Ankitkumar242 <ankitku843103@gmail.com>
2026-08-25 22:07:16 +0530

    add withdrawal

--- bank.py
+++ bank.py
@@ -25,6 +25,27 @@ class Bank:
                         
         else:
             print("Enter valid Acoount number!")
+
+    def withdrawal(self):
+        insert_acc_no =int(input("enter account number:"))
+
+        if insert_acc_no == self.account_no:
+            print("AVAILIBLE")
+            print("You have rights to withdrawal amount.")
+            amount = int(input("Enter the Amount:"))
+            if amount >= 0: 
+                self.balance -= amount
+                print("Rupees",amount,("has withdrawal in your account."))
+                print("Updated balance",(self.balance))
+
+            else:
+                print("enter valid amount !")
+        else:
+                    print("Enter valid Acoount number!")        
+
+
+
+
         
 
 
@@ -33,7 +54,9 @@ class Bank:
 account1=Bank("sahil",3421,10000,1234)
 account1.display_account()
 account1.deposit()
+account1.withdrawal()
 
 # account2=Bank("Nikhil",9065,65000,1234)
 # account2.display_account()
 # account2.deposit()
+# withdrawal (Ankit)
